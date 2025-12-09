cd11dfabc5c6188c98290567a6abc50b46f65d38
MrAsh-007 <mrash2006@gmail.com>
2025-12-09 12:29:27 +0500

    More-Less added

---
+++ JS-Side Assignments/More-Less/app.js
@@ -0,0 +1,19 @@
+function moreFn(e){
+    para.innerText = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nisi aspernatur recusandae, error sunt rerum dicta quisquam repellat quod repellendus perferendis dolore mollitia nesciunt minima vitae incidunt facere vel! Accusamus!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nisi aspernatur recusandae, error sunt rerum dicta quisquam repellat quod repellendus perferendis dolore mollitia nesciunt minima vitae incidunt facere vel! Accusamus!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nisi aspernatur recusandae, error sunt rerum dicta quisquam repellat quod repellendus perferendis dolore mollitia nesciunt minima vitae incidunt facere vel! Accusamus!"
+    btn.innerText = "See Less"
+}
+
+
+function lessFn(e){
+    para.innerText = "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+    btn.innerText = "See More"
+}
+
+
+function expand(){
+    if(btn.innerText == "See More"){
+        return moreFn(this);
+    }else{
+        return lessFn(this);
+    }
+}
